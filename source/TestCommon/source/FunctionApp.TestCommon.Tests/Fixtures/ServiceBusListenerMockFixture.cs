e00fd544f4867afa65b0730a8e195e33865f1fe0
Dan Stenrøjl <XDAST@energinet.dk>
2021-10-14 09:40:39 +0200

    What: Remove commented code

--- source/TestCommon/source/FunctionApp.TestCommon.Tests/Fixtures/ServiceBusListenerMockFixture.cs
+++ source/TestCommon/source/FunctionApp.TestCommon.Tests/Fixtures/ServiceBusListenerMockFixture.cs
@@ -26,17 +26,7 @@ namespace Energinet.DataHub.Core.FunctionApp.TestCommon.Tests.Fixtures
         public ServiceBusListenerMockFixture(IMessageSink messageSink)
         {
             ServiceBusResource = new AzureCloudServiceBusResource<ServiceBusListenerMockServiceBusOptions>(messageSink);
-
-            ////QueueName = CreateUserPrefixedName("queue");
-            ////QueueManager = new ServiceBusQueueManager(ConnectionString, QueueName);
-            ////QueueSenderClient = QueueManager.CreateSenderClient();
-
-            ////TopicName = CreateUserPrefixedName("topic");
             SubscriptionName = ServiceBusListenerMockServiceBusOptions.SubscriptionName;
-            ////TopicManager = new ServiceBusTopicManager(ConnectionString, TopicName);
-            ////TopicSenderClient = TopicManager.CreateSenderClient();
-
-            ////SubscriptionManager = TopicManager.CreateSubscriptionManager(SubscriptionName);
         }
 
         public string ConnectionString { get; private set; }
@@ -64,15 +54,6 @@ namespace Energinet.DataHub.Core.FunctionApp.TestCommon.Tests.Fixtures
 
         public async Task InitializeAsync()
         {
-            // It seem like when you close a MessageReceiver it sometimes still take the message of queue with out completing it.
-            // So to avoid test fail we need delivery count to more than 1.
-            // LockDuration is 5 sec. so when the message is taking by a closed receiver we only have to wait 5 sec. before it is released again.
-            // Note that for debugging you maybe want to increase the time to more than 5 sec.
-            ////await QueueManager.CreateQueueAsync(2, TimeSpan.FromSeconds(5));
-
-            ////await TopicManager.CreateTopicAsync();
-            ////await SubscriptionManager.CreateSubscriptionAsync(2, TimeSpan.FromSeconds(5));
-
             await ServiceBusResource.InitializeAsync();
 
             ConnectionString = ServiceBusResource.ConnectionString;
@@ -89,17 +70,7 @@ namespace Energinet.DataHub.Core.FunctionApp.TestCommon.Tests.Fixtures
         public async Task DisposeAsync()
         {
             await QueueSenderClient!.CloseAsync();
-
-            ////await QueueManager.DeleteQueueAsync();
-            ////await QueueManager.DisposeAsync();
-
             await TopicSenderClient!.CloseAsync();
-
-            ////await TopicManager.DeleteTopicAsync();
-            ////await TopicManager.DisposeAsync();
-
-            ////await SubscriptionManager.DisposeAsync();
-
             await Client.DisposeAsync();
 
             await ServiceBusResource.DisposeAsync();
