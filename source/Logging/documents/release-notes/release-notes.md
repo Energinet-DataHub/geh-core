be3fe2edb5e0e3988cc95c784181589121f02ed4
KarstenM <xnetsrak@gmail.com>
2021-12-10 10:30:39 +0100

    initial commit

---
+++ source/Logging/documents/release-notes/release-notes.md
@@ -0,0 +1,5 @@
+# Logging Middleware for Request and Response Release notes
+
+## Version 1.0.0
+
+- Initial release.
---
+++ source/Logging/source/RequestResponseMiddleware/IRequestResponseLogging.cs
@@ -0,0 +1,36 @@
+// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Threading.Tasks;
+
+namespace Energinet.DataHub.Core.Logging.RequestResponseMiddleware
+{
+    /// <summary>
+    /// Interface for request and response logging middleware.
+    /// </summary>
+    public interface IRequestResponseLogging
+    {
+        /// <summary>
+        /// Logs request
+        /// </summary>
+        /// <returns>Work task</returns>
+        Task LogRequestAsync();
+
+        /// <summary>
+        /// Logs response
+        /// </summary>
+        /// <returns>Work task</returns>
+        Task LogResponseAsync();
+    }
+}
