fe0fe2cf6f6e80695d8f12949e4e483054ad1e86
KarstenM <xnetsrak@gmail.com>
2021-12-14 16:55:57 +0100

    change log, and init test

--- source/Logging/source/RequestResponseMiddleware.Tests/RequestResponseLoggingMiddlewareTests.cs
+++ source/Logging/source/RequestResponseMiddleware.Tests/RequestResponseLoggingMiddlewareTests.cs
@@ -13,6 +13,11 @@
 // limitations under the License.
 
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Energinet.DataHub.Core.Logging.RequestResponseMiddleware;
+using Microsoft.Azure.Functions.Worker.Http;
+using Moq;
 using Xunit;
 using Xunit.Categories;
 
@@ -21,5 +26,52 @@ namespace RequestResponseMiddleware.Tests
     [UnitTest]
     public class RequestResponseLoggingMiddlewareTests
     {
+        [Fact]
+        public async Task RequestResponseLoggingMiddleware_AllOk()
+        {
+            // Arrange
+            var blobStorage = new Mock<IRequestResponseLogging>();
+            var middleware = new RequestResponseLoggingMiddleware(blobStorage.Object);
+            var functionContext = new MockedFunctionContext();
+
+            var data = new Dictionary<string, object?>(StringComparer.OrdinalIgnoreCase)
+            {
+                { "Headers", "{\"Authorization\":\"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0In0.vVkzbkZ6lB3srqYWXVA00ic5eXwy4R8oniHQyok0QWY\"}" },
+            };
+
+            functionContext.BindingContext.Setup(x => x.BindingData)
+                .Returns(data);
+
+            SetUpTest(functionContext);
+
+            // Act
+            await middleware.Invoke(functionContext, _ => Task.CompletedTask).ConfigureAwait(false);
+
+            // Assert
+            await Task.Delay(1);
+        }
+
+        private void SetUpTest(MockedFunctionContext functionContext)
+        {
+            var httpRequest = new MockedHttpRequestData(functionContext);
+            var responseData = httpRequest.HttpResponseData;
+            httpRequest.SetResponseHeaderCollection(new HttpHeadersCollection(
+                new List<KeyValuePair<string, string>>() { new ("TestId", "200") }));
+
+            var invocationFeatures = new MockedFunctionInvocationFeatures();
+            invocationFeatures.Set(new IFunctionBindingsFeature()
+            {
+                InvocationResult = responseData,
+                InputData = new Dictionary<string, object> { { "request", httpRequest.HttpRequestData } },
+            });
+            functionContext.SetInvocationFeatures(invocationFeatures);
+        }
+
+        private class IFunctionBindingsFeature
+        {
+            public HttpResponseData InvocationResult { get; set; }
+
+            public IReadOnlyDictionary<string, object> InputData { get; set; }
+        }
     }
 }
