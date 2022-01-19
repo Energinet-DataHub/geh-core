cac8136c062abe193379f7d7a81f00b51523487f
Thomas Bach Andersen <mail@thomasbach.dk>
2022-01-19 10:12:43 +0100

    Add ServiceBusActorMiddleware

---
+++ source/FunctionApp/source/Common/Middleware/Helpers/ServiceBusActorParser.cs
@@ -0,0 +1,48 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Linq;
+using System.Text.Json;
+using Energinet.DataHub.Core.FunctionApp.Common.Abstractions.Actor;
+
+namespace Energinet.DataHub.Core.FunctionApp.Common.Middleware.Helpers
+{
+    public static class ServiceBusActorParser
+    {
+        public static Actor FromString(string userIdentity)
+        {
+            if (string.IsNullOrWhiteSpace(userIdentity)) throw new ArgumentNullException(nameof(userIdentity));
+            return JsonSerializer.Deserialize<Actor>(userIdentity) ?? throw new JsonException(nameof(userIdentity));
+        }
+
+        public static Actor FromDictionaryString(string inputText, string propertyKey)
+        {
+            if (string.IsNullOrWhiteSpace(inputText)) throw new ArgumentNullException(nameof(inputText));
+            if (string.IsNullOrWhiteSpace(propertyKey)) throw new ArgumentNullException(nameof(propertyKey));
+
+            var inputJsonDocument = JsonDocument.Parse(inputText);
+            var resultJsonProperty = inputJsonDocument.RootElement
+                .EnumerateObject()
+                .FirstOrDefault(e => e.Name.Equals(propertyKey, StringComparison.Ordinal));
+
+            if (resultJsonProperty.Value.ValueKind == JsonValueKind.Undefined)
+            {
+                throw new InvalidOperationException("Current actor should not be null");
+            }
+
+            return FromString(resultJsonProperty.Value.ToString() ?? string.Empty);
+        }
+    }
+}
