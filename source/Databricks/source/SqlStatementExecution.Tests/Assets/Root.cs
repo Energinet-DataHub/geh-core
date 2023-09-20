e7e83b269c323458d198e2f5f83d9be78744c993
Søren Søndergaard <s.sondergaard@icloud.com>
2023-09-20 09:11:23 +0200

    Move configuration of client to where it's registered (#236)
    * Use HttpClientFactory to configure HTTP client
    * Set base address in test client
    
    ---------
    
    Co-authored-by: HenrikSommer <xheso@energinet.dk>

--- source/Databricks/source/SqlStatementExecution.Tests/Assets/Root.cs
+++ source/Databricks/source/SqlStatementExecution.Tests/Assets/Root.cs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-namespace Energinet.DataHub.Core.Databricks.SqlStatementExecutionTests.Assets;
+namespace Energinet.DataHub.Core.Databricks.SqlStatementExecution.Tests.Assets;
 
 internal class Root
 {
