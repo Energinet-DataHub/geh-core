366cb31e9fccb28830981d056cfaf5d8975a68ba
Alexander <alexanderharrow@hotmail.com>
2023-09-21 10:11:56 +0200

    Added health checks (Databricks).

---
+++ source/Databricks/source/HealthChecks/Extensions/DependencyInjection/HealthCheckExtensions.cs
@@ -0,0 +1,62 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Net.Http.Headers;
+using Energinet.DataHub.Core.Databricks.Diagnostics.HealthChecks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Energinet.DataHub.Core.Databricks.Extensions.DependencyInjection;
+
+public static class HealthCheckExtensions
+{
+    public static IServiceCollection AddDatabricksSqlStatementExecutionHealthCheck(
+        this IServiceCollection serviceCollection,
+        string warehouseId,
+        string workspaceToken,
+        string workspaceUrl)
+    {
+        return AddSqlStatementExecutionInner(serviceCollection, warehouseId, workspaceToken, workspaceUrl);
+    }
+
+    private static IServiceCollection AddSqlStatementExecutionInner(
+        IServiceCollection serviceCollection,
+        string warehouseId,
+        string workspaceToken,
+        string workspaceUrl)
+    {
+        serviceCollection.AddOptions<DatabricksOptions>().Configure(options =>
+        {
+            options.WarehouseId = warehouseId;
+            options.WorkspaceToken = workspaceToken;
+            options.WorkspaceUrl = workspaceUrl;
+        });
+
+        serviceCollection.AddScoped<ISqlStatementClient, T>();
+        serviceCollection.AddHttpClient<ISqlStatementClient, T>(client =>
+        {
+            client.BaseAddress = new Uri(workspaceUrl);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", workspaceToken);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json");
+        });
+
+        serviceCollection.AddScoped<IDatabricksSqlResponseParser, DatabricksSqlResponseParser>();
+        serviceCollection.AddScoped<IDatabricksSqlStatusResponseParser, DatabricksSqlStatusResponseParser>();
+        serviceCollection.AddScoped<IDatabricksSqlChunkResponseParser, DatabricksSqlChunkResponseParser>();
+        serviceCollection.AddScoped<IDatabricksSqlChunkDataResponseParser, DatabricksSqlChunkDataResponseParser>();
+
+        return serviceCollection;
+    }
+}
