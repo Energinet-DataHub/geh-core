cc2973316b7ebfa8119e2a2825403de71cf16581
Søren Søndergaard <s.sondergaard@icloud.com>
2023-10-25 14:47:08 +0200

    Remove unused stopwatch instance

--- source/Databricks/source/SqlStatementExecution/DatabricksSqlWarehouseQueryExecutor.cs
+++ source/Databricks/source/SqlStatementExecution/DatabricksSqlWarehouseQueryExecutor.cs
@@ -86,7 +86,6 @@ public sealed class DatabricksSqlWarehouseQueryExecutor
     {
         var strategy = format.GetStrategy(_options);
         var request = strategy.GetStatementRequest(statement);
-        var sw = Stopwatch.StartNew();
         var response = await request.WaitForSqlWarehouseResultAsync(_httpClient, StatementsEndpointPath);
 
         if (_httpClient.BaseAddress == null) throw new InvalidOperationException();
@@ -98,14 +97,12 @@ public sealed class DatabricksSqlWarehouseQueryExecutor
 
         foreach (var chunk in response.manifest.chunks)
         {
-            sw.Restart();
             var uri = StatementsEndpointPath +
                       $"/{response.statement_id}/result/chunks/{chunk.chunk_index}?row_offset={chunk.row_offset}";
             var chunkResponse = await _httpClient.GetFromJsonAsync<ManifestChunk>(uri);
 
             if (chunkResponse?.external_links == null) continue;
 
-            sw.Restart();
             await using var stream = await _externalHttpClient.GetStreamAsync(chunkResponse.external_links[0].external_link);
 
             await foreach (var row in strategy.ExecuteAsync(stream, response))
