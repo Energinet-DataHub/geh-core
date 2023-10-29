5afd46e977c0a1b0ea02b64d05858e7ca296a8bf
Søren Søndergaard <s.sondergaard@icloud.com>
2023-10-29 21:49:42 +0100

    Remove unused "usings"

--- source/Databricks/source/SqlStatementExecution.IntegrationTests/Client/Statements/QueryPerson.cs
+++ source/Databricks/source/SqlStatementExecution.IntegrationTests/Client/Statements/QueryPerson.cs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Energinet.DataHub.Core.Databricks.SqlStatementExecution.Statement;
-
 namespace Energinet.DataHub.Core.Databricks.SqlStatementExecution.IntegrationTests.Client.Statements;
 
 public class QueryPerson : DatabricksStatement
