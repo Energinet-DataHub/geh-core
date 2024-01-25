fcf3ade84cc9fb22e944c1611ef186589b0ad4f0
Jonas Daugaard Møller <66109423+jonasdmoeller@users.noreply.github.com>
2024-01-25 14:14:17 +0100

    cleanup

--- source/TestCommon/source/FunctionApp.TestCommon/EventHub/ResourceProvider/EventHubConsumerGroupBuilder.cs
+++ source/TestCommon/source/FunctionApp.TestCommon/EventHub/ResourceProvider/EventHubConsumerGroupBuilder.cs
@@ -13,7 +13,6 @@
 // limitations under the License.
 
 using System.Threading.Tasks;
-using Energinet.DataHub.Core.FunctionApp.TestCommon.ServiceBus.ResourceProvider;
 
 namespace Energinet.DataHub.Core.FunctionApp.TestCommon.EventHub.ResourceProvider;
 
