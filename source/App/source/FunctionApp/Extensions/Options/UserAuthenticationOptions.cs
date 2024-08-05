a2c6aa4bf08b1b6e1d95664b8295030a46a5db90
Dan Stenrøjl <51327761+dstenroejl@users.noreply.github.com>
2024-08-05 15:12:29 +0200

    Update source/App/source/FunctionApp/Extensions/Options/UserAuthenticationOptions.cs
    Co-authored-by: Ebbe Knudsen <xedkn@energinet.dk>

--- source/App/source/FunctionApp/Extensions/Options/UserAuthenticationOptions.cs
+++ source/App/source/FunctionApp/Extensions/Options/UserAuthenticationOptions.cs
@@ -29,7 +29,7 @@ public class UserAuthenticationOptions
     public string ExternalMetadataAddress { get; set; } = string.Empty;
 
     /// <summary>
-    /// The id of the backend-for-frontend app, which the access tokens are issue for. The audience claim in the tokens are expected to have this value.
+    /// The Microsoft Entra id of the backend-for-frontend app, which the access tokens are issue for. The audience claim in the tokens are expected to have this value.
     /// </summary>
     public string BackendBffAppId { get; set; } = string.Empty;
 
