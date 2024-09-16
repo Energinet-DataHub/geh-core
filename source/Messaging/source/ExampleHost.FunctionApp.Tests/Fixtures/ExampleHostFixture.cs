2b956e8bd8feb66b57dd30047d693f0e7980356b
Dan Stenrøjl <XDAST@energinet.dk>
2024-09-16 15:27:59 +0200

    Integration test of subscribing to integration event

---
+++ source/Messaging/source/ExampleHost.FunctionApp.Tests/Fixtures/ExampleHostFixture.cs
@@ -0,0 +1,96 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Diagnostics.CodeAnalysis;
+using Energinet.DataHub.Core.FunctionApp.TestCommon;
+using Energinet.DataHub.Core.FunctionApp.TestCommon.Azurite;
+using Energinet.DataHub.Core.FunctionApp.TestCommon.Configuration;
+using Energinet.DataHub.Core.FunctionApp.TestCommon.FunctionAppHost;
+using Energinet.DataHub.Core.FunctionApp.TestCommon.ServiceBus.ResourceProvider;
+using Energinet.DataHub.Core.Messaging.Communication.Extensions.Options;
+using Microsoft.Extensions.Configuration;
+
+namespace ExampleHost.FunctionApp.Tests.Fixtures;
+
+public class ExampleHostFixture : FunctionAppFixture
+{
+    public ExampleHostFixture()
+    {
+        AzuriteManager = new AzuriteManager(true);
+        IntegrationTestConfiguration = new IntegrationTestConfiguration();
+        ServiceBusResourceProvider = new ServiceBusResourceProvider(
+            TestLogger,
+            IntegrationTestConfiguration.ServiceBusFullyQualifiedNamespace);
+    }
+
+    /// <summary>
+    /// Topic resource for integration events.
+    /// </summary>
+    [NotNull]
+    public TopicResource? TopicResource { get; private set; }
+
+    private AzuriteManager AzuriteManager { get; }
+
+    private IntegrationTestConfiguration IntegrationTestConfiguration { get; }
+
+    private ServiceBusResourceProvider ServiceBusResourceProvider { get; }
+
+    protected override void OnConfigureHostSettings(FunctionAppHostSettings hostSettings)
+    {
+        const string csprojname = "ExampleHost.FunctionApp";
+        var buildConfiguration = GetBuildConfiguration();
+
+        hostSettings.FunctionApplicationPath = $"..\\..\\..\\..\\{csprojname}\\bin\\{buildConfiguration}\\net8.0";
+    }
+
+    protected override void OnConfigureEnvironment()
+    {
+        Environment.SetEnvironmentVariable("FUNCTIONS_WORKER_RUNTIME", "dotnet-isolated");
+        // Storage emulator
+        Environment.SetEnvironmentVariable("AzureWebJobsStorage", AzuriteManager.FullConnectionString);
+        // Application Insights
+        Environment.SetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING", IntegrationTestConfiguration.ApplicationInsightsConnectionString);
+        // ServiceBus Namespace
+        Environment.SetEnvironmentVariable($"{ServiceBusNamespaceOptions.SectionName}__{nameof(ServiceBusNamespaceOptions.FullyQualifiedNamespace)}", IntegrationTestConfiguration.ServiceBusFullyQualifiedNamespace);
+    }
+
+    protected override async Task OnInitializeFunctionAppDependenciesAsync(IConfiguration localSettingsSnapshot)
+    {
+        // Storage emulator
+        AzuriteManager.StartAzurite();
+
+        // ServiceBus entities
+        TopicResource = await ServiceBusResourceProvider
+            .BuildTopic("integration-events")
+            .SetEnvironmentVariableToTopicName($"{IntegrationEventsOptions.SectionName}__{nameof(IntegrationEventsOptions.TopicName)}")
+            .AddSubscription("subscription")
+            .SetEnvironmentVariableToSubscriptionName($"{IntegrationEventsOptions.SectionName}__{nameof(IntegrationEventsOptions.SubscriptionName)}")
+            .CreateAsync();
+    }
+
+    protected override async Task OnDisposeFunctionAppDependenciesAsync()
+    {
+        AzuriteManager.Dispose();
+        await ServiceBusResourceProvider.DisposeAsync();
+    }
+
+    private static string GetBuildConfiguration()
+    {
+#if DEBUG
+        return "Debug";
+#else
+            return "Release";
+#endif
+    }
+}
---
+++ source/Messaging/source/ExampleHost.FunctionApp.Tests/Integration/IntegrationEventsSubscriptionTests.cs
@@ -0,0 +1,58 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Azure.Messaging.ServiceBus;
+using Energinet.DataHub.Core.FunctionApp.TestCommon;
+using Energinet.DataHub.Core.FunctionApp.TestCommon.FunctionAppHost;
+using ExampleHost.FunctionApp.Functions;
+using ExampleHost.FunctionApp.Tests.Fixtures;
+using Xunit.Abstractions;
+
+namespace ExampleHost.FunctionApp.Tests.Integration;
+
+[Collection(nameof(ExampleHostCollectionFixture))]
+public class IntegrationEventsSubscriptionTests : FunctionAppTestBase<ExampleHostFixture>, IAsyncLifetime
+{
+    public IntegrationEventsSubscriptionTests(ExampleHostFixture fixture, ITestOutputHelper testOutputHelper)
+        : base(fixture, testOutputHelper)
+    {
+    }
+
+    public Task InitializeAsync()
+    {
+        Fixture.HostManager.ClearHostLog();
+        return Task.CompletedTask;
+    }
+
+    public Task DisposeAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    [Fact]
+    public async Task IntegrationEvent_WhenSend_ShouldTriggerIntegrationEventListener()
+    {
+        // Arrange
+        var serviceBusMessage = new ServiceBusMessage()
+        {
+            MessageId = "messageId",
+        };
+
+        // Act
+        await Fixture.TopicResource.SenderClient.SendMessageAsync(serviceBusMessage);
+
+        // Assert
+        await Fixture.HostManager.AssertFunctionWasExecutedAsync(nameof(IntegrationEventListener));
+    }
+}
