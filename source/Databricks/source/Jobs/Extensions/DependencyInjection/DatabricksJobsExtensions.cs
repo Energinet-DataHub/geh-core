94432844266dfd50973e8faa24a1d220a24c36fe
Søren Søndergaard <s.sondergaard@icloud.com>
2025-03-12 10:57:15 +0100

    Update Jobs client with token provider

--- source/Databricks/source/Jobs/Extensions/DependencyInjection/DatabricksJobsExtensions.cs
+++ source/Databricks/source/Jobs/Extensions/DependencyInjection/DatabricksJobsExtensions.cs
@@ -18,6 +18,7 @@ using Energinet.DataHub.Core.Databricks.Jobs.Abstractions;
 using Energinet.DataHub.Core.Databricks.Jobs.Client;
 using Energinet.DataHub.Core.Databricks.Jobs.Configuration;
 using Energinet.DataHub.Core.Databricks.Jobs.Constants;
+using Energinet.DataHub.Core.Databricks.Jobs.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
@@ -31,6 +32,39 @@ public static class DatabricksJobsExtensions
     /// </summary>
     /// <returns>IServiceCollection containing elements needed to request Databricks Jobs API.</returns>
     public static IServiceCollection AddDatabricksJobs(this IServiceCollection serviceCollection, IConfiguration configuration, long timeout = 30)
+        => AddDatabricksJobs(serviceCollection, configuration, TokenProvider.WorkspaceTokenProvider, timeout);
+
+    /// <summary>
+    /// Adds the <see cref="IJobsApiClient"/> and required options to the service collection.
+    /// </summary>
+    /// <param name="serviceCollection">The service collection to add the services to.</param>
+    /// <param name="configuration">The configuration to bind the options to.</param>
+    /// <param name="tokenProvider">The token provider to use for authentication.</param>
+    /// <param name="timeout">The timeout for HTTP requests, in seconds. Default is 30 seconds.</param>
+    /// <returns>IServiceCollection containing elements needed to request Databricks Jobs API.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">If <see cref="TokenProvider"/> is not a known value</exception>
+    public static IServiceCollection AddDatabricksJobs(
+        this IServiceCollection serviceCollection,
+        IConfiguration configuration,
+        TokenProvider tokenProvider,
+        long timeout = 30)
+    {
+        Action<IServiceCollection> config = tokenProvider switch {
+            TokenProvider.WorkspaceTokenProvider => s => s.AddSingleton<ITokenProvider, WorkspaceTokenProvider>(),
+            TokenProvider.ServicePrincipalTokenProvider => s => s.AddSingleton<ITokenProvider, ServicePrincipalTokenProvider>(),
+            TokenProvider.AzureCliTokenProvider => s => s.AddSingleton<ITokenProvider, AzureCliTokenProvider>(),
+            _ => throw new ArgumentOutOfRangeException(nameof(tokenProvider), tokenProvider, null),
+        };
+
+        config(serviceCollection);
+
+        return ConfigureDatabricksSqlStatementExecutionDependencies(serviceCollection, configuration, timeout);
+    }
+
+    private static IServiceCollection ConfigureDatabricksSqlStatementExecutionDependencies(
+        IServiceCollection serviceCollection,
+        IConfiguration configuration,
+        long timeout)
     {
         serviceCollection.AddSingleton<IJobsApiClient, JobsApiClient>();
 
@@ -39,6 +73,7 @@ public static class DatabricksJobsExtensions
             .Bind(configuration)
             .ValidateDataAnnotations();
 
+        serviceCollection.AddTransient<AuthenticateRequestWithToken>();
         serviceCollection
             .AddHttpClient(
                 HttpClientNameConstants.DatabricksJobsApi,
@@ -48,16 +83,34 @@ public static class DatabricksJobsExtensions
                     var url = new Uri(new Uri(options.WorkspaceUrl), "api/");
                     httpClient.BaseAddress = url;
                     httpClient.Timeout = TimeSpan.FromSeconds(timeout);
-                    httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", options.WorkspaceToken);
                     httpClient.DefaultRequestHeaders.Accept.Clear();
                     httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                     httpClient.DefaultRequestHeaders.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
                 })
+            .AddHttpMessageHandler<AuthenticateRequestWithToken>()
             .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
-                {
-                    AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
-                });
+            {
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
+            });
 
         return serviceCollection;
     }
+}
+
+public enum TokenProvider
+{
+    /// <summary>
+    /// This is the legacy token provider. Reading a token from configuration.
+    /// </summary>
+    WorkspaceTokenProvider,
+
+    /// <summary>
+    /// Using a service principal to authenticate requests.
+    /// </summary>
+    ServicePrincipalTokenProvider,
+
+    /// <summary>
+    /// Using Azure CLI to authenticate requests when running integration tests.
+    /// </summary>
+    AzureCliTokenProvider,
 }
---
+++ source/Databricks/source/Jobs/Http/ServicePrincipalTokenProvider.cs
@@ -0,0 +1,33 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Azure.Core;
+using Azure.Identity;
+
+namespace Energinet.DataHub.Core.Databricks.Jobs.Http;
+
+/// <inheritdoc cref="ITokenProvider"/>
+public class ServicePrincipalTokenProvider : ITokenProvider
+{
+    public async Task<string> GetTokenAsync(CancellationToken cancellationToken = default)
+    {
+        var credential = new ManagedIdentityCredential();
+
+        // The scope is a fixed value for Databricks in Azure
+        var tokenRequestContext = new TokenRequestContext(["2ff814a6-3304-4ab8-85cb-cd0e6f879c1d/.default"]);
+        var token = await credential.GetTokenAsync(tokenRequestContext, cancellationToken).ConfigureAwait(false);
+
+        return token.Token;
+    }
+}
