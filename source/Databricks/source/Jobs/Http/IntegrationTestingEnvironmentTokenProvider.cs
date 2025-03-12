c259208511f493f68b856560a2f7fc68c9f72cd6
Søren Søndergaard <s.sondergaard@icloud.com>
2025-03-12 12:16:22 +0100

    Copy of integration testing

--- source/Databricks/source/Jobs/Http/IntegrationTestingEnvironmentTokenProvider.cs
+++ source/Databricks/source/Jobs/Http/IntegrationTestingEnvironmentTokenProvider.cs
@@ -21,11 +21,20 @@ public class IntegrationTestingEnvironmentTokenProvider : ITokenProvider
 {
     public async Task<string> GetTokenAsync(CancellationToken cancellationToken = default)
     {
-        var credential = new ChainedTokenCredential(
-            new EnvironmentCredential(),
-            new AzureCliCredential(),
-            new VisualStudioCredential(),
-            new VisualStudioCodeCredential());
+        var credential = new DefaultAzureCredential(new DefaultAzureCredentialOptions
+        {
+            // Here we disable authentication mechanisms that is not used for Integration Test environment
+            // See also: https://learn.microsoft.com/en-us/dotnet/api/overview/azure/identity-readme?view=azure-dotnet#defaultazurecredential
+            ExcludeEnvironmentCredential = false,
+            ExcludeWorkloadIdentityCredential = true,
+            ExcludeManagedIdentityCredential = true,
+            ExcludeVisualStudioCredential = false,
+            ExcludeVisualStudioCodeCredential = true,
+            ExcludeAzureCliCredential = false,
+            ExcludeAzurePowerShellCredential = true,
+            ExcludeAzureDeveloperCliCredential = true,
+            ExcludeInteractiveBrowserCredential = true,
+        });
 
         // The scope is a fixed value for Databricks in Azure
         var tokenRequestContext = new TokenRequestContext(["2ff814a6-3304-4ab8-85cb-cd0e6f879c1d/.default"]);
