eedb967ce16a38b01f28c2a7c50bd2d4074e335d
Dan Stenrøjl <XDAST@energinet.dk>
2025-05-16 15:07:40 +0200

    Skip test

--- source/App/source/Common.Tests/Identity/AuthorizationHeaderProviderTests.cs
+++ source/App/source/Common.Tests/Identity/AuthorizationHeaderProviderTests.cs
@@ -42,8 +42,7 @@ public class AuthorizationHeaderProviderTests
         token.Audiences.Should().Contain(SubsystemAuthenticationOptionsForTests.ApplicationIdUri);
     }
 
-    ////[Fact(Skip = "This test is not consistently true, because the test might run at a time where the token is refreshed on the CI environment.")]
-    [Fact]
+    [Fact(Skip = "This test is not consistently true, because the test might run at a time where the token is refreshed on the CI environment.")]
     public async Task Given_ReusedSutInstance_When_CreateAuthorizationHeaderIsCalledMultipleTimes_Then_ReturnedHeaderContainsSameTokenBecauseTokenCacheIsUsed()
     {
         // Arrange
