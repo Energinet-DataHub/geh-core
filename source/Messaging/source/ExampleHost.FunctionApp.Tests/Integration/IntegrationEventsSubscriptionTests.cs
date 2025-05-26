923491c5d2696185802c655ef58c73e0cb51f38d
Dan Stenrøjl <XDAST@energinet.dk>
2025-05-26 13:30:33 +0200

    Usings

--- source/Messaging/source/ExampleHost.FunctionApp.Tests/Integration/IntegrationEventsSubscriptionTests.cs
+++ source/Messaging/source/ExampleHost.FunctionApp.Tests/Integration/IntegrationEventsSubscriptionTests.cs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Azure.Storage.Blobs;
 using Energinet.DataHub.Core.FunctionApp.TestCommon;
 using Energinet.DataHub.Core.FunctionApp.TestCommon.FunctionAppHost;
 using Energinet.DataHub.Core.Messaging.Communication;
